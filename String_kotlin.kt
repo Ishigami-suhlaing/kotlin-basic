1feb35150e762ec1d3c8aaad30bc21808bc75e43
Ishigami Suhlaing <suhlaing23096@gmail.com>
2025-04-04 13:31:08 +0630

    Create String_kotlin.kt

---
+++ String_kotlin.kt
@@ -0,0 +1,9 @@
+var name:String = "Kotlin.android"
+print(name)
+print(name.uppercase())
+print(name.length)
+print(name.substringBefore("."))
+print(name.substringAfter("."))
+
+var res = name.split(" ")
+print(res)
