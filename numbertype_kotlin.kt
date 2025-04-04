c7cbcabe181f6374c9e85a7371beca34e9f57c94
Ishigami-suhlaing <suhlaing23096@gmail.com>
2025-04-04 12:54:31 +0630

    Create numbertype_kotlin.kt

---
+++ numbertype_kotlin.kt
@@ -0,0 +1,56 @@
+println("Hello Kotlin Number Data Type")
+/*
+Byte
+size:8 bits
+-128 to 127(2^7)
+*/
+var b:Byte = 123;
+print("Byte:$b");
+
+/*
+Short
+size:16 bits
+-32768 to 32767(2
+*/
+var s:Short = 12345;
+print("Short:$s");
+
+/*
+Int
+size: 32 bits
+(2^31)
+* */
+var i:Int = 1234567890
+print("Int:$i")
+
+/*
+Long
+size: 64 bits
+(2^63)
+*/
+
+var l:Long = 1234567890123456789
+print("Long:$l")
+
+/*
+Float
+size:32 bits
+significant bits:24
+exponent bits:8
+decimal digits:6-7
+*/
+
+var f:Float = 3.4F
+print("Float:$f")
+
+/*
+Double
+size:64 bits
+significant bits: 53
+exponent bits:11
+decimal digits:15-16
+ */
+
+var d:Double = 2.7
+print("Double:$d")
+
