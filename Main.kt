59ea45fa5644559c1b03bf78a15b873541c0956e
Su Hlaing <suhlaing23096@gmail.com>
2025-05-04 16:45:04 +0630

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,32 @@
+package com.example.courseraadvancedkotlin
+
+data class OrderItem(val food:String, val price:Double)
+
+class TaxCalculator{
+    companion object {
+
+        private const val salesTaxPercentage:Double = 0.15
+        private var orderAmount:Double = 0.00
+
+        fun getTaxAmountForOrderItems(orderItemList:List<OrderItem>): Double{
+            var totalTaxAmount: Double = 0.00
+            val orderAmount = orderItemList.sumOf { it.price }
+
+            totalTaxAmount = orderAmount * salesTaxPercentage
+            return totalTaxAmount
+        }
+    }
+}
+
+fun main(){
+    val orderItemList:List<OrderItem> = listOf(
+        OrderItem("Burger", 8.00),
+        OrderItem("Fries", 4.00),
+        OrderItem("Soda",2.00),
+        OrderItem("Ice Cream", 4.00)
+    )
+
+    val totalTaxAmount = TaxCalculator.getTaxAmountForOrderItems(orderItemList)
+
+    print(totalTaxAmount)
+}
